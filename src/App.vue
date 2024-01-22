0ee3b0f55475ea8ee82780a23853a63b3f12b48f
Danny <hhyoyo789@gmail.com>
2024-01-22 17:11:19 +0800

    feat: 6

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,20 @@
 <template>
-  <div><h1>App</h1></div>
+  <div>
+    <el-button type="primary" size="default" :icon="Plus">666</el-button>
+    <el-button type="success" size="small" :icon="Edit">777</el-button>
+    <div class="demo-pagination-block">
+      <div class="demonstration">All combined</div>
+      <el-pagination
+        :page-sizes="[100, 200, 300, 400]"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="400"
+      />
+    </div>
+  </div>
 </template>
 
-<script>
-for (let i = 0; i < 70; i++) {
-  console.log(i)
-}
+<script setup lang="ts">
+import { Plus, Edit } from '@element-plus/icons-vue'
 </script>
 
 <style></style>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+//@ts-ignore
+import zhTW from 'element-plus/dist/locale/zh-tw.mjs'
+
+const app = createApp(App)
+app.use(ElementPlus, { locale: zhTW })
+app.mount('#app')
